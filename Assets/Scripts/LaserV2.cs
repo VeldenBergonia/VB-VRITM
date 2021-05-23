7a17ae9caedf26207851e6a23ab6625702ed9e08
VeldenBergonia <50306900+VeldenBergonia@users.noreply.github.com>
2021-05-24 09:16:50 +1000

    aaaa

---
+++ Assets/Scripts/LaserV2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserV2 : MonoBehaviour
+{
+    private LineRenderer lr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, new Vector3(0, 0, hit.distance));
+            }
+        }
+        else
+        {
+            lr.SetPosition(1, new Vector3(0, 0, 5000));
+        }
+    }
+}
