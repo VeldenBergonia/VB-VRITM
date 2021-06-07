db2ccfc9fdcd2a7bd5c254b2a74ec371635ab59d
VeldenBergonia <50306900+VeldenBergonia@users.noreply.github.com>
2021-06-07 10:00:25 +1000

    ai

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+	private NavMeshAgent _nav;
+	private Transform _player;
+
+	void Start()
+	{
+		_nav = GetComponent<NavMeshAgent>();
+		_player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	void Update()
+	{
+		_nav.SetDestination(_player.position);
+	}
+}
