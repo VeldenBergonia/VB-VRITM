f7e3ebda968bf69b50711cdb1ac4b3011810e481
VeldenBergonia <50306900+VeldenBergonia@users.noreply.github.com>
2021-06-17 10:02:53 +1000

    update

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,20 +7,60 @@ public class Gun : MonoBehaviour
 
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 4f;
 
-    public Camera fpsCam; 
+    public int maxAmmo = 12;
+    private int currentAmmo = -1;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+
+    private float nextTimeToFire = 0f;
+
+    void Start ()
+    {
+        if (currentAmmo == -1)
+            currentAmmo = maxAmmo;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
         {
+            StartCoroutine(Reload());
+            return;
+        }
+          
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
+    IEnumerator Reload ()
+    {
+        isReloading = true;
+        Debug.Log("Reloading Ammo");
+
+        yield return new WaitForSeconds(reloadTime);
+        
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     void Shoot ()
     {
+        muzzleFlash.Play();
+
+        currentAmmo--;
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
