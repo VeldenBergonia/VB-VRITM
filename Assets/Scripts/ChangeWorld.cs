430c5451dfdd9ee929dd6f1b3afaa49359c3b05e
VeldenBergonia <50306900+VeldenBergonia@users.noreply.github.com>
2021-06-25 09:23:11 +1000

    menu test transition and main game testing

---
+++ Assets/Scripts/ChangeWorld.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeWorld : MonoBehaviour
+{
+    public string TargetScene;
+
+    public void NewScene()
+    {
+        SceneManager.LoadScene(TargetScene, LoadSceneMode.Single);
+    }
+
+    public void HowTo()
+    {
+        SceneManager.LoadScene(TargetScene, LoadSceneMode.Single);
+    }
+    
+    public void QuitNow()
+    {
+        Application.Quit();
+    }
+}
